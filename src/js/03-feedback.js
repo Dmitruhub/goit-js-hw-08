b7af0bd2fa5a52828f58f7632270d9b324c53c2d
Dmitruhub <dmitrumail@gmail.com>
2022-10-06 19:37:20 +0300

    commit two

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,33 +4,31 @@ const input = document.querySelector('input');
 const message = document.querySelector('textarea');
 const submitButton = document.querySelector('button');
 const LOCALSTORAGE_KEY = 'feedback-form-state';
-const saveData = {
-  email: form.elements.email.value,
-  message: form.elements.message.value,
-};
 
 updateForm();
-form.addEventListener('submit', onSubmit);
-form.addEventListener('input', throttle(onInput, 500));
-function onInput(evt) {
-  evt.preventDefault();
-  localStorage.setItem('feedback-form-state', JSON.stringify(saveData));
-}
 
 function updateForm() {
-  if (localStorage.getItem(LOCALSTORAGE_KEY)) {
-    const parsedForm = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+  const parsedForm = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+  if (parsedForm) {
     form.elements.email.value = parsedForm.email;
     form.elements.message.value = parsedForm.message;
   }
 }
 
+form.addEventListener('submit', onSubmit);
+form.addEventListener('input', throttle(onInput, 500));
+const saveData = {};
+function onInput(evt) {
+  // const saveData = {
+  //   email: form.elements.email.value,
+  //   message: form.elements.message.value,
+  // };
+  saveData[evt.target.name] = evt.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(saveData));
+}
+
 function onSubmit(evt) {
   evt.preventDefault();
-  const saveData = {
-    email: form.elements.email.value,
-    message: form.elements.message.value,
-  };
   const parsedForm = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
   if (!form.elements.email.value || !form.elements.message.value) {
     alert('The fields is not filling!');
